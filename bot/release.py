1f8cc026fe7feb1e23f2c2e9b5373774a972b94c
alimazhabe20-hub <alimazhabe20@gmail.com>
2026-09-09 16:33:37 +0330

    Add files via upload

--- bot/release.py
+++ bot/release.py
@@ -1,6 +1,6 @@
 """Release metadata and lightweight runtime information for Rooze Ziba."""
 APP_NAME = "Rooze Ziba / ALIMJ"
-VERSION = "50.0.0"
+VERSION = "51.0.0"
 RELEASE_CHANNEL = "production"
 
 
