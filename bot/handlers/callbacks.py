86e37fa8eadd1a487150d3c8c3e1f4d8736e2211
alimazhabe20-hub <alimazhabe20@gmail.com>
2026-09-23 18:54:44 +0330

    Add files via upload

--- bot/handlers/callbacks.py
+++ bot/handlers/callbacks.py
@@ -10,18 +10,4 @@ load_modular_part(__file__, 'callbacks_parts/part_999_core_legacy.py')
 
 
 _legacy_button_handler = button_handler
-async def button_handler(*args, **kwargs):
-    if args:
-        update = args[0]
-        try:
-            data = update.callback_query.data or ""
-        except Exception:
-            data = ""
-        if data.startswith("buy:"):
-            from bot.features.market.crypto_opportunity import handle_crypto_opportunity_callback
-            handled = await handle_crypto_opportunity_callback(
-                update, args[1] if len(args) > 1 else kwargs.get("context")
-            )
-            if handled:
-                return
-    return await _legacy_button_handler(*args, **kwargs)
+async def button_handler(*args, **kwargs): return await _legacy_button_handler(*args, **kwargs)
